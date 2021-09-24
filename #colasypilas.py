a79453511aec0037d1430250f20626387dee8819
EDX350 <89174510+EDX350@users.noreply.github.com>
2021-09-23 22:17:07 -0400

    All the structures
    aqui esta el ejemplo de colas, pilas, listas y arbol binario en python

---
+++ #colasypilas.py
@@ -0,0 +1,43 @@
+#colas
+frutas = []
+frutas.append('banana')
+frutas.append('uvas')
+frutas.append('mangos')
+frutas.append('Naranja')
+
+
+first_item = frutas.pop(0)
+print(first_item)
+
+
+first_item = frutas.pop(0)
+print(first_item)
+
+
+print(frutas) 
+
+
+#------------------------------------------------------
+
+
+#pilas
+letras = []
+
+
+letras.append('c')
+letras.append('a')
+letras.append('t')
+letras.append('g')
+
+
+last_item = letras.pop()
+print(last_item)
+
+
+last_item = letras.pop()
+print(last_item)
+
+
+print(letras) 
+
+
